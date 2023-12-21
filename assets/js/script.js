73e96c4aee40779eec3db5ca1da49ccba380d7b3
simonemoretti94 <simone.moretti.944@gmail.com>
2023-12-21 12:05:06 +0100

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+//script
+const tags = [
+    {
+        id: 1,
+        title: 'Scoperta di una nuova specie di papera di gomma',
+        content: 'Un breve articolo sulla recente scoperta di una specie di papera di gomma mai vista prima.',
+        tags: ['geo', 'tech'],
+        author: 'Diana Rossi',
+        published: '2023-02-11',
+    },
+    {
+        id: 2,
+        title: 'Esplorando le profondità marine: il mistero degli abissi',
+        content: 'Esplorando le profondità marine: il mistero degli abissi',
+        tags: ['viaggi', 'geo'],
+        author: 'Fabio Mari',
+        published: '2023-03-14',
+    },
+    {
+        id: 3,
+        title: 'Viaggio culinario: alla scoperta dei sapori perduti',
+        content: 'Esplorazione di tradizioni culinarie dimenticate e la ricerca di sapori autentici',
+        tags: ['cucina'],
+        author: 'Marta Bianchi',
+        published: '2023-04-20',
+    },
+    {
+        id: 4,
+        title: 'Arte moderna: oltre i confini convenzionali',
+        content: 'Un\'n analisi delle tendenze e delle sfide nell\'arte contemporanea, con interviste ad artisti emergenti.',
+        tags: ['arte', 'tech'],
+        author: 'Gabriele Neri',
+        published: '2023-05-29',
+    },  
+];
+
